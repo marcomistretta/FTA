8cac3a0beede34b37db43774ebd283c081f686f6
manu <cascio713@gmail.com>
2021-03-09 00:56:03 +0100

    aggiunto template Triplet OwO

--- src/main/java/src/swe/smft/utilities/Triplet.java
+++ src/main/java/src/swe/smft/utilities/Triplet.java
@@ -1,5 +1,25 @@
 package src.swe.smft.utilities;
 
-public enum Triplet {
-    // TODO TBD
+public class Triplet<T, S, U> {
+    private T element1;
+    private S element2;
+    private U element3;
+
+    public Triplet(T t, S s, U u) {
+        element1 = t;
+        element2 = s;
+        element3 = u;
+    }
+
+    public T getElement1() {
+        return element1;
+    }
+
+    public S getElement2() {
+        return element2;
+    }
+
+    public U getElement3() {
+        return element3;
+    }
 }
